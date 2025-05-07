0ec8475e2418137d8139252025d652828ac8e6e9
xOnlineSavage93 <129092693+xOnlineSavage93@users.noreply.github.com>
2025-05-06 21:30:40 -0400

    Posion throwable
    Cory - working on throwable potion, with script and messed with a gas asset

---
+++ Assets/Scripts/Player/PotionImpact.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PotionImpact : MonoBehaviour
+{
+    public GameObject explosionEffect; 
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (explosionEffect != null)
+        {
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+        }
+
+        
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            
+        }
+
+        Destroy(gameObject); 
+    }
+}
--- Assets/Scripts/Player/Throwable.cs
+++ Assets/Scripts/Player/Throwable.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Throwable : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject potionPrefab;
+    public Transform tempPlayerWeapon;
+    public float throwForce = 10f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.J)) // Press J to throw
+        {
+            ThrowPotion();
+        }
+    }
+
+    void ThrowPotion()
+    {
+        if (tempPlayerWeapon == null || potionPrefab == null)
+        {
+            Debug.LogWarning("Missing reference for potionPrefab or tempPlayerWeapon.");
+            return;
+        }
+
+        Vector2 throwDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - tempPlayerWeapon.position).normalized;
+
+        GameObject potion = Instantiate(potionPrefab, tempPlayerWeapon.position, Quaternion.identity);
+        Rigidbody2D rb = potion.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(throwDirection * throwForce, ForceMode2D.Impulse);
+        }
     }
 }
