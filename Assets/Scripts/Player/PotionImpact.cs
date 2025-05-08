cc6d795eac60d8a8dc5feefb3c37d07779fab4e0
jr7799 <jayrossi7799@gmail.com>
2025-05-08 16:20:53 -0400

    stuff

--- Assets/Scripts/Player/PotionImpact.cs
+++ Assets/Scripts/Player/PotionImpact.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 public class PotionImpact : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject pop;
+    public Vector3 position;
     private void OnDestroy()
     {
-        Instantiate(prefab, transform.position, Quaternion.identity);
+        Instantiate(pop, position, Quaternion.identity);
+        Instantiate(prefab, position, Quaternion.identity);
     }
 }
