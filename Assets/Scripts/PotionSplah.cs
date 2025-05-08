e1d4e0f1c6e219b60aee965317e33ea673c9ecd5
jr7799 <jayrossi7799@gmail.com>
2025-05-08 13:53:08 -0400

    new

---
+++ Assets/Scripts/PotionSplah.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PotionSplah : MonoBehaviour
+{
+    public float destroyTime = 3.5f;
+    private void Start()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+    public float damage = 5;
+    [SerializeField] bool isCriticalHit;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isCriticalHit = Random.Range(0, 100) < 30;
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemyHealth enemy = collision.gameObject.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                if (!isCriticalHit)
+                    enemy.TakeDamage(damage, false);
+                else
+                    enemy.TakeDamage(damage * 1.35f, true);
+            }
+        }
+    }
+
+}
